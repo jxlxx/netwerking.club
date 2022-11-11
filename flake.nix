d8a8eb4b63ba54f8b50ef51e2f2334cbc044382d
jxlxx <jul14+git@proton.me>
2022-11-11 13:31:06 -0500

    spacing

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-
   outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
